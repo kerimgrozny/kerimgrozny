794ad7cdc3467de7a0e1b16b7ddb6900d5b66c04
kerimg <kerimgrozny@gmail.com>
2015-08-30 15:45:30 +0300

    form changed

--- public/site_order.php
+++ public/site_order.php
@@ -2,20 +2,39 @@
     <div class="row" xmlns="http://www.w3.org/1999/html">
         <h1>Заполните форму пожалуйста</h1>
         <div class="col-xs-12 col-md-6">
-            <form action="site_order.php" role="form"  method="POST">
+            <form class="form-horizontal" role="form">
                 <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    <label class="control-label col-sm-2">Сайт:</label>
+                    <div class="col-sm-4">
+                        <select class="form-control" name="SiteType">
+                            <option value="4900">Сайт визитка</option>
+                            <option value="volvo">Информационный сайт</option>
+                            <option value="saab">Промо-сайт</option>
+                            <option value="mercedes">Корпоративный сайт</option>
+                            <option value="audi">Интернет-магазин</option>
+                        </select>
+                    </div>
                 </div>
                 <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                    <label class="control-label col-sm-2" for="pwd">Password:</label>
+                    <div class="col-sm-4">
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                    </div>
                 </div>
-                <div class="checkbox">
-                    <label><input type="checkbox"> Remember me</label>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <div class="checkbox">
+                            <label><input type="checkbox"> Remember me</label>
+                        </div>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-default">Submit</button>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </div>
+                </div>
             </form>
+
         </div>
         <div class="col-xs-12 col-md-6">
             <img class="img-responsive" src="images/serviceImages/websiteorder.jpg">
