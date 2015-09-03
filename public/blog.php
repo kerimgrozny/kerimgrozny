01b1e14188d4290082f42048a8a5551ababaace6
kerimg <kerimgrozny@gmail.com>
2015-09-03 21:23:59 +0300

    major changes added

--- public/blog.php
+++ public/blog.php
@@ -4,42 +4,15 @@
 <div class="container" id="content" xmlns="http://www.w3.org/1999/html"><!--content container start-->
     <div class="row"><!--content row start-->
         <h1>Блог</h1>
-        <div class="col-xs-12 col-md-3">
-            <ul>
-                <?php
-                    $query  = "SELECT * FROM blog ";
-                    $query .= "WHERE Visible = 1";
-                    $subject_set = mysqli_query($connection, $query);
-                ?>
-                <?php
-                    while($subject = mysqli_fetch_assoc($subject_set)) {
-                ?>
-                    <li>
-                        <?php echo $subject["Name"]; ?>
-                        <ul>
-                            <?php
-                                $query  = "SELECT * FROM blog_page WHERE SubjectID = {$subject["SubjectID"]}";
-                                $page_set = mysqli_query($connection, $query);
-                            ?>
-                            <?php
-                                while($page = mysqli_fetch_assoc($page_set)) {
-                            ?>
-                                <li><?php echo $page["Content"]; ?></li>
-                            <?php
-                                }
-                            ?>
-                        </ul>
-                    </li>
-                <?php
-                }
-                ?>
-            </ul>
 
-
-
+        <div class="col-xs-2 col-lg-2 bg-success">
+            <h3 class="pull-center">Темы</h3>
 
         </div>
-
+        <div class="col-xs-10 col-lg-10 bg-info">
+            <h3 class="pull-center">Дискуссия</h3>
+            
+        </div>
         <div class="col-xs-12 col-md-9">
 
         </div>
